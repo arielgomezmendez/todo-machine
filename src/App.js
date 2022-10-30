354bac8b3a9d5fce04a24f9a038b1926cbddc41d
Ariel <arielgomezmendez94@gmail.com>
2022-10-30 11:06:41 -0400

    Create component: TodoList, CreateButton

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
-import TodoCounter  from "./TodoCounter";
-import TodoSearch  from "./TodoSearch";
+import TodoCounter from "./TodoCounter";
+import TodoSearch from "./TodoSearch";
+import TodoList from "./TodoList";
+import CreateButton from "./CreateButton";
 
 function App() {
   return (
     <React.Fragment>
-      <TodoCounter/>
+      <TodoCounter />
 
-      <TodoSearch/>
+      <TodoSearch />
       <input placeholder="cebolla"></input>
-      {/*<TodoList>
-        <TodoItem/>
-      </TodoList>
-      <CreateButton/>*/}
+      <TodoList>{/*<TodoItem/> */}</TodoList>
+      <CreateButton />
       <button>+</button>
     </React.Fragment>
   );
