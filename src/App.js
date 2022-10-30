7aeb1560ec7a0e9ef9745caacd1060efeea36d74
Ariel <arielgomezmendez94@gmail.com>
2022-10-30 14:14:45 -0400

    Layout tha main structure of app

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import TodoCounter from "./TodoCounter";
 import TodoSearch from "./TodoSearch";
 import TodoList from "./TodoList";
 import CreateButton from "./CreateButton";
+import TodoItem from "./TodoItem";
 
+const todos = [
+  { text: "Cortar cebolla", completed: false },
+  { text: "Tormar el curso de intro a react", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+];
 function App() {
   return (
     <React.Fragment>
       <TodoCounter />
-
       <TodoSearch />
       <input placeholder="cebolla"></input>
-      <TodoList>{/*<TodoItem/> */}</TodoList>
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem text={todo.text} />
+        ))}
+      </TodoList>
       <CreateButton />
       <button>+</button>
     </React.Fragment>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const TodoList = () => {
+const TodoList = (props) => {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <ul>
+        {props.children}
+      </ul>
+    </section>
   )
 }
 
