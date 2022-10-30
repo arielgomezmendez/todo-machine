ca2425ce01403c0d172cf898ada2f8e6cc4ebada
Ariel <arielgomezmendez94@gmail.com>
2022-10-30 14:38:31 -0400

    Create the css files

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import "./TodoCounter.css";
 
 const TodoCounter = () => {
   return (
     <div>
-      <h2>Haz completado 2 de 3 todos</h2>
+      <h2 className="TodoCounter">Haz completado 2 de 3 todos</h2>
     </div>
-  )
-}
+  );
+};
 
-export default TodoCounter
+export default TodoCounter;
