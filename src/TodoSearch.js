71f1e05676d4493b59d46454dd0c91133d9dc5ca
Ariel <arielgomezmendez94@gmail.com>
2022-10-31 22:11:19 -0400

    Create the onSearchValue function in the TodoSearch component

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import "./TodoSearch.css"
+import React from "react";
+import "./TodoSearch.css";
 
 const TodoSearch = () => {
+  const onSearchValue = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <div>
-      <input className='TodoSearch' placeholder="cebolla"></input>
+      <input
+        className="TodoSearch"
+        placeholder="cebolla"
+        onChange={onSearchValue}
+      ></input>
     </div>
-  )
-}
+  );
+};
 
-export default TodoSearch
+export default TodoSearch;
