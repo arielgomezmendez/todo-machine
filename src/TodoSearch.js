52a9f78d68d0a5932b96c923c8a4326b5f8321c8
Ariel <arielgomezmendez94@gmail.com>
2022-11-02 16:03:58 -0400

    Create the state searchValue in the TodoSearch component

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,17 +1,22 @@
 import React from "react";
 import "./TodoSearch.css";
-
+import { useState } from "react";
 const TodoSearch = () => {
-  const onSearchValue = (event) => {
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const onSearchValueChange = (event) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   };
   return (
     <div>
       <input
         className="TodoSearch"
         placeholder="cebolla"
-        onChange={onSearchValue}
+        onChange={onSearchValueChange}
+        value={searchValue}
       ></input>
+      <p>{searchValue}</p>
     </div>
   );
 };
