7e545fffbeaa219df1f202d6540f9865e13f6aad
Ariel <arielgomezmendez94@gmail.com>
2022-11-02 21:02:06 -0400

    Create logic for operation of the search engine

--- src/App.js
+++ src/App.js
@@ -13,16 +13,33 @@ const defaultTodos = [
 ];
 function App() {
   const [todos, setTodos] = React.useState(defaultTodos);
+  // State of search
   const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter((todo) => todo.completed == true).length;
   const totalTodos = todos.length;
+
+  // Create array to store all todos, to avoid to many renders in the application,
+  // when the user  makes the search
+  let searchedTodos = [];
+
+  //logic for operation of the search engine
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
   return (
     <React.Fragment>
       <TodoCounter total={totalTodos} completed={completedTodos} />
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
-        {todos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem key={todo.text} text={todo.text} />
         ))}
       </TodoList>
