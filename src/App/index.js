5a22396b460d5c423f5decbcee194ced2f1c7c1f
Ariel <arielgomezmendez94@gmail.com>
2022-12-06 21:54:53 -0500

    Save the todos in localstorage of browser

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,26 @@ import React from "react";
 import "./App.css";
 import { AppUI } from "./AppUI";
 
-const defaultTodos = [
+/*const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
   { text: "Tormar el curso de intro a react", completed: false },
   { text: "Llorar con la llorona", completed: false },
-];
+];*/
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  // Traemos nuestros TODOs almacenados
+  const localStorageTodos = localStorage.getItem("TODOS_V1");
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    // Si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos uno con un array vacío
+    localStorage.setItem("TODOS_V1", JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    // Si existen TODOs en el localStorage los regresamos como nuestros todos
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+  // Guardamos nuestros TODOs del localStorage en nuestro estado
+  const [todos, setTodos] = React.useState(parsedTodos);
   // State of search
   const [searchValue, setSearchValue] = React.useState("");
 
@@ -29,12 +42,23 @@ function App() {
       return todoText.includes(searchText);
     });
   }
+  // Creamos la función en la que actualizaremos nuestro localStorage
+  const saveTodos = (newTodos) => {
+    // Convertimos a string nuestros TODOs
+    const stringifiedTodos = JSON.stringify(newTodos);
+     // Los guardamos en el localStorage
+    localStorage.setItem("TODOS_V1", stringifiedTodos);
+    // Actualizamos nuestro estado
+    setTodos(newTodos);
+  };
+
   //Function to complete to-do
   const completeTodos = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    // Cada que el usuario interactúe con nuestra aplicación se guardarán los TODOs con nuestra nueva función
+    saveTodos(newTodos);
   };
 
   //Function to delete to-do
@@ -42,7 +66,8 @@ function App() {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    // Cada que el usuario interactúe con nuestra aplicación se guardarán los TODOs con nuestra nueva función
+    saveTodos(newTodos);
   };
 
   return (
